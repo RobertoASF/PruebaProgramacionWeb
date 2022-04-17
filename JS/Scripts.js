5b8b66f3667f97a79925cfacd8dc6283eb0b18c4
kevin Oyarzun <kev.oyarzun@duocuc.cl>
2022-04-17 17:44:17 -0400

    cambios en scripts

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -7,3 +7,32 @@ Clickbutton.forEach(btn => {
 })
 
 
+function addToCarritoItem(e){
+  const button = e.target
+  const item = button.closest('.card')
+  const itemTitle = item.querySelector('.card-title').textContent;
+  const itemPrice = item.querySelector('.precio').textContent;
+  const itemImg = item.querySelector('.card-img-top').src;
+
+  const newItem = {
+    title: itemTitle,
+    precio: itemPrice,
+    img: itemImg,
+    cantidad: 1
+  }
+  addItemCarrito(newItem)
+
+}
+
+
+function addItemCarrito(newItem){
+
+
+    carrito.push(newItem)
+    renderCarrito()
+}
+
+
+function renderCarrito(){
+   console.log(carrito)
+    }
