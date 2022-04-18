257d38afa474ae8e3bec692b41f7180a7c5e7d13
kevin Oyarzun <kev.oyarzun@duocuc.cl>
2022-04-17 21:23:06 -0400

    se cambio el codigo carrito

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -20,19 +20,43 @@ function addToCarritoItem(e){
     img: itemImg,
     cantidad: 1
   }
-  addItemCarrito(newItem)
 
+  addItemCarrito(newItem)
 }
 
 
 function addItemCarrito(newItem){
 
 
+
     carrito.push(newItem)
+
     renderCarrito()
+  } 
+  
+  
+  function renderCarrito(){
+    tbody.innerHTML = ''
+    carrito.map(item => {
+      const tr = document.createElement('tr')
+      tr.classList.add('ItemCarrito')
+      const Content = `
+      
+     <th scope="row">1</th>
+            <td class="table__productos">
+                <img src=${item.img}  alt="">
+                <h6 class="title">${item.title}</h6>
+              </td>
+              <td class="table__price"><p>${item.precio}</p></td>
+              <td class="table__cantidad">
+                <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+                <button class="delete btn btn-danger">x</button>
+              </td>
+      
+    `
+    tr.innerHTML = Content;
+    tbody.append(tr)
+    })
+     
 }
-
-
-function renderCarrito(){
-   console.log(carrito)
-    }
+  
