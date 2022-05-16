e08aecd9f51eb08845452ff97d56dd234bf9728e
unknown <roberto.asf@gmail.com>
2022-05-16 18:36:12 -0400

    se corrgen id de los botones

--- JS/tienda.js
+++ JS/tienda.js
@@ -1,18 +1,18 @@
 /*Enlaces a documentos php*/
 $(document).ready(function() {
-    $('#div-btn5').on('click', function() {
+    $('#div-btnt03').on('click', function() {
         $('.navbar-nav li').removeClass('active');
         $("#central").load('inc/aves.php');
         return false;
     });
 
-    $('#div-btn6').on('click', function() {
+    $('#div-btnt02').on('click', function() {
         $('.navbar-nav li').removeClass('active');
         $("#central").load('inc/gatos.php');
         return false;
     });
 
-    $('#div-btn7').on('click', function() {
+    $('#div-btnt01').on('click', function() {
         $('.navbar-nav li').removeClass('active');
         $("#central").load('inc/perros.php');
         return false;
