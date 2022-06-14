3b7db91883375fa8eeb4128f84c15a4cd22cd157
Roberto Sanchez <roberto.asf@gmail.com>
2022-06-13 21:53:18 -0400

    se arega login

--- tienda/models.py
+++ tienda/models.py
@@ -1,6 +1,9 @@
 import code
 from distutils.command.upload import upload
+from mailbox import NoSuchMailboxError
 from os import ST_MANDLOCK
+from statistics import correlation
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
@@ -11,7 +14,15 @@ class Producto(models.Model):
     precio      = models.IntegerField(default=0)
     cat         = models.CharField(max_length=50)
     stock       = models.IntegerField(default=0)
-    imagen      = models.ImageField(upload_to="productos", null=True)
+    imagen      = models.ImageField(upload_to="productos")
 
+    def __str__(self):
+        return self.nombre
+
+class Users(models.Model):
+    id_usuario  = models.IntegerField(unique=True)
+    name        = models.CharField(max_length=50)
+    correo      = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.nombre
